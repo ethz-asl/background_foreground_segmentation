65db5b2733383888557dad92fed9fd62946a92d6
Skype-line <shenkaiyue1997@gmail.com>
2020-11-18 17:13:59 +0100

    run yapf

--- src/bfseg/data/nyu/Nyu_depth_v2_labeled.py
+++ src/bfseg/data/nyu/Nyu_depth_v2_labeled.py
@@ -25,12 +25,13 @@ _URL = 'http://horatio.cs.nyu.edu/mit/silberman/nyu_depth_v2/nyu_depth_v2_labele
 
 
 def convert_mat(data):
-  assert data.attrs['MATLAB_class'].decode()=='char'
-  string_array=np.array(data).ravel()
+  assert data.attrs['MATLAB_class'].decode() == 'char'
+  string_array = np.array(data).ravel()
   string_array = ''.join([chr(x) for x in string_array])
   string_array = string_array.replace('\x00', '')
   return string_array
-  
+
+
 class NyuDepthV2Labeled(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for Nyu_depth_v2_labeled dataset."""
 
@@ -39,6 +40,7 @@ class NyuDepthV2Labeled(tfds.core.GeneratorBasedBuilder):
       # '1.0.0': 'Initial release.',
       '2.0.0': 'different scenes for train/test',
   }
+
   # MANUAL_DOWNLOAD_INSTRUCTIONS = 1
 
   def _info(self) -> tfds.core.DatasetInfo:
@@ -48,14 +50,14 @@ class NyuDepthV2Labeled(tfds.core.GeneratorBasedBuilder):
         builder=self,
         description=_DESCRIPTION,
         features=tfds.features.FeaturesDict({
-          'image': tfds.features.Image(shape=(480, 640, 3), dtype=tf.uint8),
-          # 'depth': tfds.features.Tensor(shape=(480, 640), dtype=tf.float16),
-          'label': tfds.features.Tensor(shape=(480, 640), dtype=tf.uint8), 
+            'image': tfds.features.Image(shape=(480, 640, 3), dtype=tf.uint8),
+            # 'depth': tfds.features.Tensor(shape=(480, 640), dtype=tf.float16),
+            'label': tfds.features.Tensor(shape=(480, 640), dtype=tf.uint8),
         }),
         # features=tfds.features.FeaturesDict({
         #   'image': tfds.features.Image(shape=(48, 64, 3), dtype=tf.uint8),
         #   # 'depth': tfds.features.Tensor(shape=(480, 640), dtype=tf.float16),
-        #   'label': tfds.features.Tensor(shape=(48, 64), dtype=tf.uint16), 
+        #   'label': tfds.features.Tensor(shape=(48, 64), dtype=tf.uint16),
         # }),
         # If there's a common (input, target) tuple from the
         # features, specify them here. They'll be used if
@@ -76,17 +78,17 @@ class NyuDepthV2Labeled(tfds.core.GeneratorBasedBuilder):
             name=tfds.Split.TRAIN,
             # These kwargs will be passed to _generate_examples
             gen_kwargs={
-              'dataset_path': download_dir,
-              # 'dataset_path': dl_manager.manual_dir,
-              'scene_type':'kitchen',
+                'dataset_path': download_dir,
+                # 'dataset_path': dl_manager.manual_dir,
+                'scene_type': 'kitchen',
             },
         ),
         tfds.core.SplitGenerator(
             name=tfds.Split.TEST,
             gen_kwargs={
-              'dataset_path': download_dir,
-              # 'dataset_path': dl_manager.manual_dir,
-              'scene_type':'bedroom',
+                'dataset_path': download_dir,
+                # 'dataset_path': dl_manager.manual_dir,
+                'scene_type': 'bedroom',
             },
         ),
     ]
@@ -95,30 +97,34 @@ class NyuDepthV2Labeled(tfds.core.GeneratorBasedBuilder):
     """Yields examples."""
     # TODO(Nyu_depth_v2_labeled): Yields (key, example) tuples from the dataset
     h5py = tfds.core.lazy_imports.h5py
-    with h5py.File(dataset_path,'r') as f:
+    with h5py.File(dataset_path, 'r') as f:
       Images = f['images']
       # Depths = f['depths']
       Labels = f['labels']
-      Images = np.array(f['images'],dtype=f['images'].dtype).T.squeeze()
+      Images = np.array(f['images'], dtype=f['images'].dtype).T.squeeze()
       # Depths=np.array(f['depths'],dtype=f['images'].dtype).T.squeeze()
-      Labels = np.array(f['labels'],dtype=f['labels'].dtype).T.squeeze()
+      Labels = np.array(f['labels'], dtype=f['labels'].dtype).T.squeeze()
       refs = f['#refs#']
       cell = []
       for ref in f['sceneTypes']:
         row = []
         for r in ref:
-            entry = refs.get(r)
-            row.append(convert_mat(entry))
+          entry = refs.get(r)
+          row.append(convert_mat(entry))
         cell.append(row)
       for i in range(Images.shape[-1]):
         # Label_expand = np.expand_dims(Labels[:,:,i], axis=2)
-        if cell[0][i]==scene_type:
-          label = Labels[:,:,i]
-          combine_label = np.logical_or(label==4,(np.logical_or(label==11,label==21))).astype(np.uint8)
-          yield str(i).zfill(4), {'image':Images[:,:,:,i],
-                  # 'depth':Depths[:,:,i],
-                  # 'label':mask}
-                  'label':combine_label}
+        if cell[0][i] == scene_type:
+          label = Labels[:, :, i]
+          combine_label = np.logical_or(
+              label == 4,
+              (np.logical_or(label == 11, label == 21))).astype(np.uint8)
+          yield str(i).zfill(4), {
+              'image': Images[:, :, :, i],
+              # 'depth':Depths[:,:,i],
+              # 'label':mask}
+              'label': combine_label
+          }
         # yield str(i).zfill(4), {'image':Image_resize,
         #         'label':Label_resize}
 
@@ -135,27 +141,27 @@ class NyuDepthV2Labeled(tfds.core.GeneratorBasedBuilder):
 #   cell.append(row)
 
 
-# def count_scenes_num():
-#   h5py = tfds.core.lazy_imports.h5py
-#   f=h5py.File('nyu_depth_v2_labeled.mat','r')
-#   refs = f['#refs#']
-#   cell = []
-#   for ref in f['sceneTypes']:
-#     row = []
-#     for r in ref:
-#         entry = refs.get(r)
-#         row.append(convert_mat(entry))
-#     cell.append(row)
-#   scene_type_list = ['kitchen', 'office', 'bathroom', 'living_room', 'bedroom', 'bookstore', 'cafe', 'furniture_store', \
-#                 'study_room', 'classroom', 'computer_lab', 'conference_room', 'dinette', 'excercise_room', 'foyer', \
-#                 'home_office', 'home_storage', 'indoor_balcony', 'laundry_room', 'office_kitchen', 'playroom', \
-#                 'printer_room', 'reception_room', 'study', 'basement', 'dining_room', 'student_lounge']
-#                  #[225.,  78., 121., 221., 383.,  36.,   5.,  27.,  \
-#                   #7.,  49.,   6., 5.,   4.,   3.,   4., \
-#                   # 50.,   5.,   2.,   3.,  10.,  31.,   \
-#                   # 3., 17.,  25.,   7., 117.,   5.]
-#   cnt = np.zeros(len(scene_type_list))
-#   for i, scene_type in enumerate(scene_type_list):
-#     for j in cell[0]:
-#       if j==scene_type:
-#         cnt[i]+=1
+def count_scenes_num():
+  h5py = tfds.core.lazy_imports.h5py
+  f = h5py.File('nyu_depth_v2_labeled.mat', 'r')
+  refs = f['#refs#']
+  cell = []
+  for ref in f['sceneTypes']:
+    row = []
+    for r in ref:
+      entry = refs.get(r)
+      row.append(convert_mat(entry))
+    cell.append(row)
+  scene_type_list = ['kitchen', 'office', 'bathroom', 'living_room', 'bedroom', 'bookstore', 'cafe', 'furniture_store', \
+                'study_room', 'classroom', 'computer_lab', 'conference_room', 'dinette', 'excercise_room', 'foyer', \
+                'home_office', 'home_storage', 'indoor_balcony', 'laundry_room', 'office_kitchen', 'playroom', \
+                'printer_room', 'reception_room', 'study', 'basement', 'dining_room', 'student_lounge']
+  #[225.,  78., 121., 221., 383.,  36.,   5.,  27.,  \
+  #7.,  49.,   6., 5.,   4.,   3.,   4., \
+  # 50.,   5.,   2.,   3.,  10.,  31.,   \
+  # 3., 17.,  25.,   7., 117.,   5.]
+  cnt = np.zeros(len(scene_type_list))
+  for i, scene_type in enumerate(scene_type_list):
+    for j in cell[0]:
+      if j == scene_type:
+        cnt[i] += 1
--- src/train_multi_segmodel.py
+++ src/train_multi_segmodel.py
@@ -1,123 +1,173 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from Nyu_depth_v2_labeled.Nyu_depth_v2_labeled import NyuDepthV2Labeled 
+from Nyu_depth_v2_labeled.Nyu_depth_v2_labeled import NyuDepthV2Labeled
 import segmentation_models as sm
 from tensorflow import keras
 import tensorflow.keras.preprocessing.image as Image
 import os
 
+
 @tf.function
 def normalize_img(image, label):
-	"""Normalizes images: `uint8` -> `float32`."""
-	# image = tf.image.resize(image,(32,64))
-	# label = tf.cast(label, tf.uint8)
-	# combine_label = tf.cast(tf.math.logical_or(tf.math.equal(label,4),(tf.logical_or(tf.math.equal(label,11),tf.math.equal(label,21)))),tf.uint8)
-	# combine_label= tf.expand_dims(combine_label,axis=2)
-	label = tf.expand_dims(label,axis=2)
-	# print(label.shape)
-	image = tf.cast(image, tf.float32) / 255.
-	# return image, combine_label
-	return image, label
+  """Normalizes images: `uint8` -> `float32`."""
+  # image = tf.image.resize(image,(32,64))
+  # label = tf.cast(label, tf.uint8)
+  # combine_label = tf.cast(tf.math.logical_or(tf.math.equal(label,4),(tf.logical_or(tf.math.equal(label,11),tf.math.equal(label,21)))),tf.uint8)
+  # combine_label= tf.expand_dims(combine_label,axis=2)
+  label = tf.expand_dims(label, axis=2)
+  # print(label.shape)
+  image = tf.cast(image, tf.float32) / 255.
+  # return image, combine_label
+  return image, label
+
 
 def create_mask(pred_mask):
-	pred_mask = tf.argmax(pred_mask, axis=-1)
-	pred_mask = pred_mask[..., tf.newaxis]
-	return pred_mask
+  pred_mask = tf.argmax(pred_mask, axis=-1)
+  pred_mask = pred_mask[..., tf.newaxis]
+  return pred_mask
+
 
 def main():
-	batch_size = 8
-	# train_ds, train_info = tfds.load('NyuDepthV2Labeled', split='train[:80%]',shuffle_files=True, as_supervised=True, with_info=True,)
-	# test_ds, test_info = tfds.load('NyuDepthV2Labeled', split='train[80%:]',shuffle_files=False, as_supervised=True, with_info=True,)
-	train_ds, train_info = tfds.load('NyuDepthV2Labeled', split='train[:80%]',shuffle_files=True, as_supervised=True, with_info=True,)
-	val_ds, val_info = tfds.load('NyuDepthV2Labeled', split='train[80%:]',shuffle_files=True, as_supervised=True, with_info=True,)
-	test_ds, test_info = tfds.load('NyuDepthV2Labeled', split='test',shuffle_files=False, as_supervised=True, with_info=True,)
-	train_ds = train_ds.map(
-	    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-	train_ds = train_ds.cache()
-	train_ds = train_ds.shuffle(int(train_info.splits['train'].num_examples*0.8))
-	# train_ds = train_ds.shuffle(int(train_info.splits['train'].num_examples*0.8))
-	train_ds = train_ds.batch(batch_size).repeat()
-	train_ds = train_ds.prefetch(tf.data.experimental.AUTOTUNE)
-	
-	val_ds = val_ds.map(
-	    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-	val_ds = val_ds.batch(batch_size)
-	val_ds = val_ds.cache()
+  batch_size = 8
+  # train_ds, train_info = tfds.load('NyuDepthV2Labeled', split='train[:80%]',shuffle_files=True, as_supervised=True, with_info=True,)
+  # test_ds, test_info = tfds.load('NyuDepthV2Labeled', split='train[80%:]',shuffle_files=False, as_supervised=True, with_info=True,)
+  train_ds, train_info = tfds.load(
+      'NyuDepthV2Labeled',
+      split='train[:80%]',
+      shuffle_files=True,
+      as_supervised=True,
+      with_info=True,
+  )
+  val_ds, val_info = tfds.load(
+      'NyuDepthV2Labeled',
+      split='train[80%:]',
+      shuffle_files=True,
+      as_supervised=True,
+      with_info=True,
+  )
+  test_ds, test_info = tfds.load(
+      'NyuDepthV2Labeled',
+      split='test',
+      shuffle_files=False,
+      as_supervised=True,
+      with_info=True,
+  )
+  train_ds = train_ds.map(normalize_img,
+                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+  train_ds = train_ds.cache()
+  train_ds = train_ds.shuffle(int(train_info.splits['train'].num_examples *
+                                  0.8))
+  # train_ds = train_ds.shuffle(int(train_info.splits['train'].num_examples*0.8))
+  train_ds = train_ds.batch(batch_size).repeat()
+  train_ds = train_ds.prefetch(tf.data.experimental.AUTOTUNE)
 
-	test_ds = test_ds.map(
-	    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-	test_ds = test_ds.batch(batch_size)
-	test_ds = test_ds.cache()
-	# test_ds = test_ds.prefetch(tf.data.experimental.AUTOTUNE)
-	callbacks = [
-				keras.callbacks.ModelCheckpoint('./best_model.h5', save_weights_only=True, save_best_only=True, mode='min'),
-    			keras.callbacks.ReduceLROnPlateau(),]
-	# for batch in train_ds:
-	# 	print(batch[1])
-	BACKBONE = "vgg16"
-	# preprocess_input = sm.get_preprocessing(BACKBONE)
+  val_ds = val_ds.map(normalize_img,
+                      num_parallel_calls=tf.data.experimental.AUTOTUNE)
+  val_ds = val_ds.batch(batch_size)
+  val_ds = val_ds.cache()
 
-	model = sm.Unet(BACKBONE,input_shape=(480, 640, 3),classes=2,activation='sigmoid')
-	# model = tf.keras.models.Sequential([
-	# 		tf.keras.layers.Flatten(input_shape=(48, 64, 3)),
-	# 		tf.keras.layers.Dense(1024,activation='relu'),
-	# 		tf.keras.layers.Dense(894, activation='softmax')
-	# 		])
-	model.summary()
-	# metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
-	model.compile(
-	    loss='sparse_categorical_crossentropy',
-		# loss=sm.losses.CategoricalFocalLoss(),
-	    optimizer=tf.keras.optimizers.Adam(0.0001),
-	    # metrics=metrics,
-		metrics='accuracy',
-	)
+  test_ds = test_ds.map(normalize_img,
+                        num_parallel_calls=tf.data.experimental.AUTOTUNE)
+  test_ds = test_ds.batch(batch_size)
+  test_ds = test_ds.cache()
+  # test_ds = test_ds.prefetch(tf.data.experimental.AUTOTUNE)
+  callbacks = [
+      keras.callbacks.ModelCheckpoint('./best_model.h5',
+                                      save_weights_only=True,
+                                      save_best_only=True,
+                                      mode='min'),
+      keras.callbacks.ReduceLROnPlateau(),
+  ]
+  # for batch in train_ds:
+  # 	print(batch[1])
+  BACKBONE = "vgg16"
+  # preprocess_input = sm.get_preprocessing(BACKBONE)
 
-	model.fit(
-	    train_ds,
-		# steps_per_epoch=int(train_info.splits['train'].num_examples*0.8)//batch_size,
-	    steps_per_epoch=train_info.splits['train'].num_examples//batch_size,
-	    epochs=10,
-		validation_data=test_ds,
-		callbacks=callbacks, 
-		)
-	model_save_dir = './checkpoints'
-	if os.path.exists(model_save_dir)==False:
-		os.mkdir(model_save_dir)
-	model.save_weights(os.path.join(model_save_dir,'model_epoch10_diffscene.ckpt'))
-	print("Evaluate on val data")
-	val_results = model.evaluate(val_ds)
-	print("val loss, val acc:", val_results) 
-	print("Evaluate on test data")
-	test_results = model.evaluate(test_ds)
-	print("test loss, test acc:", test_results)
+  model = sm.Unet(BACKBONE,
+                  input_shape=(480, 640, 3),
+                  classes=2,
+                  activation='sigmoid')
+  # model = tf.keras.models.Sequential([
+  # 		tf.keras.layers.Flatten(input_shape=(48, 64, 3)),
+  # 		tf.keras.layers.Dense(1024,activation='relu'),
+  # 		tf.keras.layers.Dense(894, activation='softmax')
+  # 		])
+  model.summary()
+  # metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+  model.compile(
+      loss='sparse_categorical_crossentropy',
+      # loss=sm.losses.CategoricalFocalLoss(),
+      optimizer=tf.keras.optimizers.Adam(0.0001),
+      # metrics=metrics,
+      metrics='accuracy',
+  )
 
-	testing_save_path = './test_result_epoch10_diffscene/'
-	validating_save_path = './val_result_epoch10_diffscene/'
-	if os.path.exists(testing_save_path)==False:
-		os.mkdir(testing_save_path)
-	if os.path.exists(validating_save_path)==False:
-		os.mkdir(validating_save_path)
-	i=0
-	for image, label in test_ds:
-		for j in range(image.shape[0]):
-			pred_label = model.predict(image)
-			Image.save_img(os.path.join(testing_save_path,str(i*batch_size+j).zfill(4)+'_image.png'), image[j])
-			Image.save_img(os.path.join(testing_save_path,str(i*batch_size+j).zfill(4)+'_trueseg.png'), label[j])
-			# print(label[j])
-			Image.save_img(os.path.join(testing_save_path,str(i*batch_size+j).zfill(4)+'_preddeg.png'), create_mask(pred_label[j]))
-			# print(create_mask(pred_label[j]))
-		print("Predicting test batch %d" %i)
-		i = i+1
-	i=0
-	for image, label in val_ds:
-		for j in range(image.shape[0]):
-			pred_label = model.predict(image)
-			Image.save_img(os.path.join(validating_save_path,str(i*batch_size+j).zfill(4)+'_image.png'), image[j])
-			Image.save_img(os.path.join(validating_save_path,str(i*batch_size+j).zfill(4)+'_trueseg.png'), label[j])
-			Image.save_img(os.path.join(validating_save_path,str(i*batch_size+j).zfill(4)+'_preddeg.png'), create_mask(pred_label[j]))
-		print("Predicting train batch %d" %i)
-		i = i+1
+  model.fit(
+      train_ds,
+      # steps_per_epoch=int(train_info.splits['train'].num_examples*0.8)//batch_size,
+      steps_per_epoch=train_info.splits['train'].num_examples // batch_size,
+      epochs=10,
+      validation_data=test_ds,
+      callbacks=callbacks,
+  )
+  model_save_dir = './checkpoints'
+  if os.path.exists(model_save_dir) == False:
+    os.mkdir(model_save_dir)
+  model.save_weights(
+      os.path.join(model_save_dir, 'model_epoch10_diffscene.ckpt'))
+  print("Evaluate on val data")
+  val_results = model.evaluate(val_ds)
+  print("val loss, val acc:", val_results)
+  print("Evaluate on test data")
+  test_results = model.evaluate(test_ds)
+  print("test loss, test acc:", test_results)
+
+  testing_save_path = './test_result_epoch10_diffscene/'
+  validating_save_path = './val_result_epoch10_diffscene/'
+  if os.path.exists(testing_save_path) == False:
+    os.mkdir(testing_save_path)
+  if os.path.exists(validating_save_path) == False:
+    os.mkdir(validating_save_path)
+  i = 0
+  for image, label in test_ds:
+    for j in range(image.shape[0]):
+      pred_label = model.predict(image)
+      Image.save_img(
+          os.path.join(testing_save_path,
+                       str(i * batch_size + j).zfill(4) + '_image.png'),
+          image[j])
+      Image.save_img(
+          os.path.join(testing_save_path,
+                       str(i * batch_size + j).zfill(4) + '_trueseg.png'),
+          label[j])
+      # print(label[j])
+      Image.save_img(
+          os.path.join(testing_save_path,
+                       str(i * batch_size + j).zfill(4) + '_preddeg.png'),
+          create_mask(pred_label[j]))
+      # print(create_mask(pred_label[j]))
+    print("Predicting test batch %d" % i)
+    i = i + 1
+  i = 0
+  for image, label in val_ds:
+    for j in range(image.shape[0]):
+      pred_label = model.predict(image)
+      Image.save_img(
+          os.path.join(validating_save_path,
+                       str(i * batch_size + j).zfill(4) + '_image.png'),
+          image[j])
+      Image.save_img(
+          os.path.join(validating_save_path,
+                       str(i * batch_size + j).zfill(4) + '_trueseg.png'),
+          label[j])
+      Image.save_img(
+          os.path.join(validating_save_path,
+                       str(i * batch_size + j).zfill(4) + '_preddeg.png'),
+          create_mask(pred_label[j]))
+    print("Predicting train batch %d" % i)
+    i = i + 1
+
+
 if __name__ == "__main__":
-	sm.set_framework('tf.keras')
-	main()
+  sm.set_framework('tf.keras')
+  main()
