9f97af61e4d2411c9a041d5a2916f14a56b82111
René Zurbrügg <zrene@student.ethz.ch>
2021-01-05 15:31:37 +0100

    linter

--- src/dataset_creator.cpp
+++ src/dataset_creator.cpp
@@ -217,7 +217,8 @@ void Creator::projectPointCloud(
   std::shared_ptr<tf::StampedTransform> t(new tf::StampedTransform);
   tf_listener->waitForTransform(camera_frame, cloud_frame, cloud->header.stamp,
                                 ros::Duration(0.4));
-  tf_listener->lookupTransform(camera_frame, cloud_frame,  cloud->header.stamp, *t);
+  tf_listener->lookupTransform(camera_frame, cloud_frame, cloud->header.stamp,
+                               *t);
 
   // Image that contains the projected pointcloud
   cv::Mat preview_img = camera_image.clone();
