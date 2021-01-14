144ab7b28386269b84c587c4a4e641c144bb78c3
Hermann <blumh@ethz.ch>
2021-01-14 22:55:40 +0100

    fast scnn training

---
+++ src/nyu_pretraining.py
@@ -0,0 +1,42 @@
+from sacred import Experiment
+import tensorflow as tf
+import pandas as pd
+import tensorflow_datasets as tfds
+
+import bfseg.data.nyu.Nyu_depth_v2_labeled
+from bfseg.utils.losses import ignorant_cross_entropy_loss
+from bfseg.utils.metrics import IgnorantMeanIoU
+from bfseg.models.fast_scnn import fast_scnn
+from bfseg.utils.utils import crop_map
+
+ex = Experiment()
+
+
+@ex.main
+def pretrain_nyu(_run, batchsize=10, learning_rate=1e-4):
+  train_data = tfds.load(
+      'NyuDepthV2Labeled', split='train',
+      as_supervised=True).map(crop_map).shuffle(1000).batch(batchsize).cache()
+  val_data = tfds.load(
+      'NyuDepthV2Labeled', split='test',
+      as_supervised=True).map(crop_map).batch(batchsize).cache()
+
+  x = tf.keras.Input(shape=train_data.element_spec[0].shape[1:])
+  out = tf.image.convert_image_dtype(x, tf.float32)
+  out = fast_scnn(out, num_downsampling_layers=1, num_classes=2)
+  model = tf.keras.Model(inputs=x, outputs=out)
+  model.compile(loss=ignorant_cross_entropy_loss,
+                optimizer=tf.keras.optimizers.Adam(learning_rate),
+                metrics=[IgnorantMeanIoU()])
+  history = model.fit(train_data.take(2), epochs=2, validation_data=val_data.take(2))
+  hist = pd.DataFrame(history.history)
+  hist['epoch'] = history.epoch
+  _run.info['final_loss'] = hist['loss'].iloc[-1]
+  _run.info['final miou'] = hist['val_ignorant_mean_io_u'].iloc[-1]
+  for _, row in hist.iterrows():
+    _run.log_scalar('training.loss', row['loss'], row['epoch'])
+    _run.log_scalar('validation.loss', row['val_loss'], row['epoch'])
+
+
+if __name__ == '__main__':
+  ex.run_commandline()
