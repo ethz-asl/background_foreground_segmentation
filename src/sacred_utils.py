d20712d5b452fda9e8d7185958bdb06764503f26
Hermann <blumh@ethz.ch>
2021-01-15 12:21:24 +0100

    style corretion for copy-pasted code

--- src/sacred_utils.py
+++ src/sacred_utils.py
@@ -3,17 +3,16 @@ import bfseg.settings as settings
 
 
 def get_observer():
-    if hasattr(settings, 'EXPERIMENT_DB_HOST') and settings.EXPERIMENT_DB_HOST:
-        print('mongo observer created', flush=True)
-        return MongoObserver.create(url='mongodb://{user}:{pwd}@{host}/{db}'.format(
-                                        host=settings.EXPERIMENT_DB_HOST,
-                                        user=settings.EXPERIMENT_DB_USER,
-                                        pwd=settings.EXPERIMENT_DB_PWD,
-                                        db=settings.EXPERIMENT_DB_NAME),
-                                    db_name=settings.EXPERIMENT_DB_NAME)
-    elif hasattr(settings, 'EXPERIMENT_STORAGE_FOLDER') \
-            and settings.EXPERIMENT_STORAGE_FOLDER:
-        return FileStorageObserver.create(settings.EXPERIMENT_STORAGE_FOLDER)
-    else:
-        raise UserWarning("No observer settings found.")
-
+  if hasattr(settings, 'EXPERIMENT_DB_HOST') and settings.EXPERIMENT_DB_HOST:
+    print('mongo observer created', flush=True)
+    return MongoObserver.create(url='mongodb://{user}:{pwd}@{host}/{db}'.format(
+        host=settings.EXPERIMENT_DB_HOST,
+        user=settings.EXPERIMENT_DB_USER,
+        pwd=settings.EXPERIMENT_DB_PWD,
+        db=settings.EXPERIMENT_DB_NAME),
+                                db_name=settings.EXPERIMENT_DB_NAME)
+  elif hasattr(settings, 'EXPERIMENT_STORAGE_FOLDER') \
+          and settings.EXPERIMENT_STORAGE_FOLDER:
+    return FileStorageObserver.create(settings.EXPERIMENT_STORAGE_FOLDER)
+  else:
+    raise UserWarning("No observer settings found.")
