"Fix" log-likelihood reduction in EWC For each image, now the log-likelihood is averaged over all the valid pixels in the batch, rather than summed. In practice, this introduces a scale down by an oom of 1e5. (Important because gradients would also be squared)

src/bfseg/cl_models/ewc_model.py
@@ -88,8 +88,8 @@ class EWC(BaseCLModel):
 
         # Sum the log-likelihoods across all pixels.
         sum_log_likelihood = -tf.keras.losses.SparseCategoricalCrossentropy(
-            reduction=tf.keras.losses.Reduction.SUM, from_logits=True)(
-                gt_y_masked, pred_y_masked)
+            reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
+            from_logits=True)(gt_y_masked, pred_y_masked)
       # Compute gradients w.r.t. weights of the model. NOTE: at this point, the
       # weights have not been updated with the new task, therefore the gradients
       # are computed at the operating point of the previous task.