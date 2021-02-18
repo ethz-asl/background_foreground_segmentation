b8b804a7abc337d8ebb875b2e1cce4e22dbe7a6f
René Zurbrügg <zrene@student.ethz.ch>
2021-02-18 10:50:27 +0100

    Created Validation Set with labels from the hive

--- src/bfseg/data/hive/__init__.py
+++ src/bfseg/data/hive/__init__.py
@@ -1,3 +1,4 @@
 """Bfseg validation set labeled (CLA and ARCHE)"""
 
 from .bfseg_validation_labeled import BfsegValidationLabeled
+from .office_rumlang_validation_labeled import OfficeRumlangValidationLabeled
