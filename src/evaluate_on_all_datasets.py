6a0653ef9a468932994862e5a09f0c8229c4dc2c
Francesco Milano <francesco.milano172@gmail.com>
2021-02-25 11:23:14 +0100

    Add utility to evaluate model on all datasets

---
+++ src/evaluate_on_all_datasets.py
@@ -0,0 +1,41 @@
+from bfseg.utils.evaluation import evaluate_model_multiple_epochs_and_datasets
+
+USER = TO_DEFINE
+
+save_folder = f"/cluster/scratch/{USER}/evaluation"
+
+model_to_evaluate = {
+    f'/cluster/scratch/{USER}/pretrained_models/'
+    'model_epoch_42_group_norm.h5':
+        42
+}
+
+datasets_to_evaluate = {
+    # NYU.
+    "NyuDepthV2Labeled": None,
+    "NyuDepthV2Labeled": "kitchen",
+    "NyuDepthV2Labeled": "bedroom",
+    # Hive labels.
+    "BfsegValidationLabeled": "CLA",
+    "OfficeRumlangValidationLabeled": "OFFICE",
+    "OfficeRumlangValidationLabeled": "RUMLANG",
+    # Pseudo-labels.
+    "BfsegCLAMeshdistLabels": None,
+    "MeshdistPseudolabels": "garage1",
+    "MeshdistPseudolabels": "garage2",
+    "MeshdistPseudolabels": "garage3",
+    "MeshdistPseudolabels": "rumlang_full",
+    "MeshdistPseudolabels": "rumlang2",
+    "MeshdistPseudolabels": "rumlang3",
+    "MeshdistPseudolabels": "office4",
+    "MeshdistPseudolabels": "office5",
+    "MeshdistPseudolabels": "office4_2302",
+    "MeshdistPseudolabels": "office4_2402",
+}
+
+accuracies, mean_ious = evaluate_model_multiple_epochs_and_datasets(
+    pretrained_dirs=[*model_to_evaluate.keys()],
+    epochs_to_evaluate=[*model_to_evaluate.values()],
+    datasets_names_to_evaluate=[*datasets_to_evaluate.keys()],
+    datasets_scenes_to_evaluate=[*datasets_to_evaluate.values()],
+    save_folder=save_folder)
