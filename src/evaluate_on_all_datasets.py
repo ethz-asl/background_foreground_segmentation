d67efac4654a4f4e5488912a96d66706d11e35d2
Francesco Milano <francesco.milano172@gmail.com>
2021-02-25 11:41:30 +0100

    Fix multi-dataset evaluation script

--- src/evaluate_on_all_datasets.py
+++ src/evaluate_on_all_datasets.py
@@ -10,32 +10,32 @@ model_to_evaluate = {
         42
 }
 
-datasets_to_evaluate = {
+datasets_to_evaluate = [
     # NYU.
-    "NyuDepthV2Labeled": None,
-    "NyuDepthV2Labeled": "kitchen",
-    "NyuDepthV2Labeled": "bedroom",
+    ("NyuDepthV2Labeled", None),
+    ("NyuDepthV2Labeled", "kitchen"),
+    ("NyuDepthV2Labeled", "bedroom"),
     # Hive labels.
-    "BfsegValidationLabeled": "CLA",
-    "OfficeRumlangValidationLabeled": "OFFICE",
-    "OfficeRumlangValidationLabeled": "RUMLANG",
+    ("BfsegValidationLabeled", "CLA"),
+    ("OfficeRumlangValidationLabeled", "OFFICE"),
+    ("OfficeRumlangValidationLabeled", "RUMLANG"),
     # Pseudo-labels.
-    "BfsegCLAMeshdistLabels": None,
-    "MeshdistPseudolabels": "garage1",
-    "MeshdistPseudolabels": "garage2",
-    "MeshdistPseudolabels": "garage3",
-    "MeshdistPseudolabels": "rumlang_full",
-    "MeshdistPseudolabels": "rumlang2",
-    "MeshdistPseudolabels": "rumlang3",
-    "MeshdistPseudolabels": "office4",
-    "MeshdistPseudolabels": "office5",
-    "MeshdistPseudolabels": "office4_2302",
-    "MeshdistPseudolabels": "office4_2402",
-}
+    ("BfsegCLAMeshdistLabels", None),
+    ("MeshdistPseudolabels", "garage1"),
+    ("MeshdistPseudolabels", "garage2"),
+    ("MeshdistPseudolabels", "garage3"),
+    ("MeshdistPseudolabels", "rumlang_full"),
+    ("MeshdistPseudolabels", "rumlang2"),
+    ("MeshdistPseudolabels", "rumlang3"),
+    ("MeshdistPseudolabels", "office4"),
+    ("MeshdistPseudolabels", "office5"),
+    ("MeshdistPseudolabels", "office4_2302"),
+    ("MeshdistPseudolabels", "office4_2402"),
+]
 
 accuracies, mean_ious = evaluate_model_multiple_epochs_and_datasets(
     pretrained_dirs=[*model_to_evaluate.keys()],
     epochs_to_evaluate=[*model_to_evaluate.values()],
-    datasets_names_to_evaluate=[*datasets_to_evaluate.keys()],
-    datasets_scenes_to_evaluate=[*datasets_to_evaluate.values()],
+    datasets_names_to_evaluate=[d[0] for d in datasets_to_evaluate],
+    datasets_scenes_to_evaluate=[d[1] for d in datasets_to_evaluate],
     save_folder=save_folder)
