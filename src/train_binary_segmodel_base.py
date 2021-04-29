94261cf7eee407aaf3a4646886c897e9cea3f525
mbrucker07 <matthias.brucker@hotmail.de>
2021-04-29 20:27:12 +0200

    GPU debug 1

--- src/train_binary_segmodel_base.py
+++ src/train_binary_segmodel_base.py
@@ -12,6 +12,22 @@ from bfseg.utils.datasets import load_datasets
 ex = Experiment()
 ex.observers.append(get_observer())
 
+### Todo: might not need this
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=13000)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
+
+
 
 @ex.main
 def run(_run, network_params, training_params, dataset_params, logging_params,
