2bceb8e24dd2982cc308a9e01b429a5f2ad662c0
mbrucker07 <matthias.brucker@hotmail.de>
2021-04-29 22:16:50 +0200

    debug GPU 2

--- src/train_binary_segmodel_base.py
+++ src/train_binary_segmodel_base.py
@@ -20,7 +20,7 @@ if gpus:
   try:
     tf.config.experimental.set_virtual_device_configuration(
         gpus[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=3000)])
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=7000)])
     logical_gpus = tf.config.experimental.list_logical_devices('GPU')
     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
   except RuntimeError as e:
