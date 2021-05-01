5c1a7055bd4b97e574d27d288d1909ff83f1c514
mbrucker07 <matthias.brucker@hotmail.de>
2021-05-01 18:24:49 +0200

    removed test

--- src/inference_node.py
+++ src/inference_node.py
@@ -88,8 +88,7 @@ def callback(pred_func, img_pubs, pointcloud, *image_msgs):
     resizeTime += time.time() - timeB
     # convert to numpy
     timeC = time.time()
-    prediction = tf.make_ndarray(prediction)
-    #prediction = prediction.numpy()
+    prediction = prediction.numpy()
     numpyTime += time.time() - timeC
     timeE = time.time()
     prediction = prediction.astype('uint8')
