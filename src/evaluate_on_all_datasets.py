79b6f7276b5edc234d5c5f37316edc0497f60f2a
Francesco Milano <francesco.milano172@gmail.com>
2021-05-20 14:25:23 +0200

    Allow to run evaluation both with and w/o FoV mask and fix hard-coding

--- src/evaluate_on_all_datasets.py
+++ src/evaluate_on_all_datasets.py
@@ -4,6 +4,10 @@ from bfseg.utils.evaluation import evaluate_model_multiple_epochs_and_datasets
 
 save_folder = PATH_TO_THE_FOLDER_THAT_SHOULD_CONTAIN_THE_EVALUATION_RESULTS
 pretrained_models_folder = PATH_TO_THE_FOLDER_WITH_THE_PRETRAINED_MODELS
+
+# Whether or not to use the FoV mask for evaluation.
+use_fov_mask = True
+
 # Set this to True to save the images with the segmentation predictions.
 save_predictions = False
 
@@ -56,4 +60,5 @@ for id_, epoch_ in id_and_epoch:
           datasets_names_to_evaluate=[d[0] for d in datasets_to_evaluate],
           datasets_scenes_to_evaluate=[d[1] for d in datasets_to_evaluate],
           save_folder=id_folder,
+          use_fov_mask=use_fov_mask,
           save_predictions=save_predictions)
