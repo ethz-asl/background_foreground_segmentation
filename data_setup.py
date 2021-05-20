c5a853ef5e7aded34a60fef2469b7f25ea08e8b8
Hermann <blumh@ethz.ch>
2021-05-20 17:18:05 +0200

    add nyu subsampled to data setup

--- data_setup.py
+++ data_setup.py
@@ -1,6 +1,7 @@
 import tensorflow_datasets as tfds
 
 import bfseg.data.nyu.Nyu_depth_v2_labeled
+import bfseg.data.nyu_subsampled
 import bfseg.data.hive.bfseg_validation_labeled
 import bfseg.data.hive.office_rumlang_validation_labeled
 import bfseg.data.meshdist.bfseg_cla_meshdist_labels
@@ -9,8 +10,8 @@ import bfseg.data.pseudolabels
 tfds.load('OfficeRumlangValidationLabeled')
 tfds.load('MeshdistPseudolabels')
 tfds.load('NyuDepthV2Labeled')
+tfds.load('nyu_subsampled')
 tfds.load("BfsegValidationLabeled")
 tfds.load("meshdist_pseudolabels")
 tfds.load("office_rumlang_validation_labeled")
 tfds.load('BfsegCLAMeshdistLabels')
-
--- src/online_learning.py
+++ src/online_learning.py
@@ -14,6 +14,7 @@ import time
 import numpy as np
 import random
 
+import bfseg.data.nyu_subsampled
 from bfseg.utils.models import create_model
 from bfseg.utils.losses import BalancedIgnorantCrossEntropyLoss
 from bfseg.utils.image_enhancement import aggregate_sparse_labels
