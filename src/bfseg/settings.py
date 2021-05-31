dc2806352178659b20aa1b6151ef1d1b70dcbcd2
mbrucker07 <matthias.brucker@hotmail.de>
2021-05-31 20:20:06 +0200

    xavier settings!

--- src/bfseg/settings.py
+++ src/bfseg/settings.py
@@ -1,4 +1,4 @@
 # settings.py 
 
-TMPDIR = '/home/matthias/data/rss_2021_data/exp_temp/'
-EXPERIMENT_STORAGE_FOLDER = '/home/matthias/data/rss_2021_data/exp_storage/'
+TMPDIR = '/home/xavier/data/rss_2021_data/exp_temp/'
+EXPERIMENT_STORAGE_FOLDER = '/home/xavier/data/rss_2021_data/exp_storage/'
