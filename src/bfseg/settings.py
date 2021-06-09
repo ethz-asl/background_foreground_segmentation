b57b704412829723b79ae194d89cb5d1cd1dca68
mbrucker07 <matthias.brucker@hotmail.de>
2021-06-10 00:25:59 +0200

    typo corrected

--- src/bfseg/settings.py
+++ src/bfseg/settings.py
@@ -1,6 +1,6 @@
 # settings.py 
 
-TMPDIR = '/media/xavier/sandik/xavier_models/exp_temp/'
+TMPDIR = '/media/xavier/sandisk/xavier_models/exp_temp/'
 EXPERIMENT_STORAGE_FOLDER = '/media/xavier/sandisk/xavier_models/exp_storage/'
 #TMPDIR = '/home/matthias/data/rss_data_2021/exp_temp/'
 #EXPERIMENT_STORAGE_FOLDER = '/home/matthias/data/rss_data_2021/exp_storage/'
